16cadf57de5fb6e20259cea8555e1ca5cb867479
HaroldBins <filay0921@gmail.com>
2025-05-03 07:51:49 +0800

    config changes

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const baseURL = process.env.REACT_APP_API_BASE_URL || "https://amshc-backend-v2.onrender.com";
+const baseURL = process.env.REACT_APP_BASE_URL || "https://amshc-backend-v2.onrender.com";
 console.log("✅ ENV loaded:", baseURL);
 
 export default {
